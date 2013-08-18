cfb92290fc4e246318cb17ea4507382c43d4e464
Trevor Harwel <trevor.harwell@gmail.com>
2013-08-18 00:10:12 -0700

    Adding needed client libraries as submodules

--- config.js
+++ config.js
@@ -1,3 +1,21 @@
 require.config({
-
+  baseUrl: './',
+  paths: {
+    jquery     : 'library/jquery/dist/jquery',
+    backbone   : 'library/backbone/backbone',
+    underscore : 'library/underscore/underscore',
+    vertebrae  : 'library/vertebrae'
+  },
+  shim: {
+    jquery: {
+      exports: 'jQuery'
+    },
+    backbone: {
+      exports: 'Backbone'
+      deps: ['jquery', 'underscore']
+    },
+    underscore: {
+      exports: '_'
+    }
+  }
 });
